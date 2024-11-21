518329c5aab3d7efb14720e78c35b8e732cfe4a9
JOHN MATHEW PUA <jmrpua@mymail.mapua.edu.ph>
2024-11-21 21:39:50 +0800

    Add project files.

---
+++ NocturnalBrews/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NocturnalBrews.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ NocturnalBrews/Startup.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Hosting;
+//using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NocturnalBrews.Models;
+using Microsoft.EntityFrameworkCore;
+//using Pajutrao_Project.Services;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        //services.AddDbContext<InternshipDbContext>(options =>
+        //options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+        
+        // Add authentication and configure cookie settings
+        //services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+        //    .AddCookie(options =>
+        //    {
+        //        options.LoginPath = "/Home/Login"; // Path to redirect for login
+        //        options.LogoutPath = "/Home/Logout"; // Path for logout
+        //        options.AccessDeniedPath = "/Home/AccessDenied"; // Redirect if access denied
+        //    });
+
+        services.AddControllersWithViews();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        // Add authentication and authorization middleware
+        //app.UseAuthentication();
+        //app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}"); // Default route, direct to Login page initially
+        });
+    }
+}
